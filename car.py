836066398d4d0ac18583318f31d9a619765e39cb
Yves Vollmeier <yves.vollmeier.code@gmail.com>
2023-08-10 15:48:53 +0200

    Add license headers

--- car.py
+++ car.py
@@ -1,5 +1,22 @@
 # Original version from https://github.com/NeuralNine/ai-car-simulation
 
+# The following license applies to all changes made:
+
+# This file is part of simulation for cars learning how to steer using a Genetic Algorithm
+# Everything is openly developed on GitHub: https://github.com/Tix3Dev/genetic_algorithm_self_driving_car
+#
+# Copyright (C) 2023  Yves Vollmeier <https://github.com/Tix3Dev>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import pygame
 import math
 import random
@@ -86,6 +103,7 @@ class Car:
         self.position[0] = min(self.position[0], self.screen.get_size()[0] - 120)
 
         # Increase Distance and Time
+*/
         self.dist_passed += self.speed
         
         # Same For Y-Position
--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+# This file is part of simulation for cars learning how to steer using a Genetic Algorithm
+# Everything is openly developed on GitHub: https://github.com/Tix3Dev/genetic_algorithm_self_driving_car
+#
+# Copyright (C) 2023  Yves Vollmeier <https://github.com/Tix3Dev>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import pygame
 
 from game import Game
